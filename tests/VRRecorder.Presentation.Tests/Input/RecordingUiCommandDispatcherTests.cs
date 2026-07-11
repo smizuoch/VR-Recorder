0879b8193694bc18b1ef3f471463956891c2985c
smizuoch <shotaro.mizuochi@proton.me>
2026-07-11 23:39:42 +0900

    test(input): require dedicated mic and mute routing

--- tests/VRRecorder.Presentation.Tests/Input/RecordingUiCommandDispatcherTests.cs
+++ tests/VRRecorder.Presentation.Tests/Input/RecordingUiCommandDispatcherTests.cs
@@ -4,6 +4,46 @@ namespace VRRecorder.Presentation.Tests.Input;
 
 public sealed class RecordingUiCommandDispatcherTests
 {
+    [Theory]
+    [InlineData(
+        UiCommandId.ToggleMicrophone,
+        UiActivationKind.DesktopKeyboard,
+        "microphone")]
+    [InlineData(
+        UiCommandId.ToggleMuteAll,
+        UiActivationKind.WristRay,
+        "mute")]
+    public async Task AudioCommandsRouteToTheirDedicatedOperations(
+        UiCommandId command,
+        UiActivationKind activationKind,
+        string expectedRoute)
+    {
+        var routed = new List<(string Route, UiActivationKind Activation)>();
+        var dispatcher = new RecordingUiCommandDispatcher(
+            (activation, cancellationToken) =>
+            {
+                routed.Add(("recording", activation));
+                return Task.CompletedTask;
+            },
+            (activation, cancellationToken) =>
+            {
+                routed.Add(("microphone", activation));
+                return Task.CompletedTask;
+            },
+            (activation, cancellationToken) =>
+            {
+                routed.Add(("mute", activation));
+                return Task.CompletedTask;
+            });
+
+        await dispatcher.DispatchAsync(
+            command,
+            activationKind,
+            CancellationToken.None);
+
+        Assert.Equal([(expectedRoute, activationKind)], routed);
+    }
+
     [Fact]
     public async Task CanonicalToggleRoutesExactlyOnce()
     {
